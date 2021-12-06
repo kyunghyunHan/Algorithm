7abf9aaf37144e1a01450d9b2a77b92705a63dff
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-12-06 21:53:36 +0900

    string

---
+++ golang/string.go
@@ -0,0 +1,22 @@
+//문자열 s는 한 개 이상의 단어로 구성되어 있습니다.
+//각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+//각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+package main
+
+import "unicode"
+
+func main(s string) string {
+	stars := []rune(s)
+	index := 0
+	for i, value := range stars {
+		if unicode.IsSpace(value) {
+			index = i + 1
+		} else if (i-index)%2 == 0 {
+			stars[i] = unicode.ToUpper(stars[i])
+		} else {
+			stars[i] = unicode.ToLower(stars[i])
+		}
+	}
+	return string(stars)
+}
